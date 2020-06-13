02e658186e899338545c5c388f1f58d3ce5ee5cd
Martin Jurkovic <mj5835@student.uni-lj.si>
2020-06-13 14:20:06 +0200

    Fixed url composing

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # RTV4D-downloader
-Downloads RTV4D files from URL
+Downloads RTV4D files from URL, as per this [URL](https://zmaga.com/content.php?id=7526)
 
 ## Instructions
 Pass the url to main.py as argument
+
+### Notice
+Because of RTV servers download sometimes doesn't work :(
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ id = re.split(r"/", sys.argv[1])[-1]
 
 # api-endpoint
 URL = "http://api.rtvslo.si/ava/getRecording/{id}?client_id=82013fb3a531d5414f478747c1aca622".format(id=id)
-# print("URL: " + URL)
+print("URL: " + URL)
 
 # extracting data in json format
 webURL = urllib.request.urlopen(URL)
@@ -23,9 +23,11 @@ data = json.loads(data.decode(encoding))
 
 server = data['response']['mediaFiles'][0]['streamers']['http']
 filename = data['response']['mediaFiles'][0]['filename']
+filename = '/'+filename if filename[0] != '/' else filename
+
 dow_name = re.split(r"/", filename)[-1]
 
-address = "{s}/{f}".format(s=server, f=filename)
+address = "{s}{f}".format(s=server, f=filename)
 # print("ADDRESS: "+ address)
 
 try:
